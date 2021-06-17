9e6c044e56769b7c5749327f8caf693d35a84937
Cherron Simes <cherron@developdbycherron.com>
2021-06-17 16:08:06 -0500

    Added CRUD and token checker functions

--- client/user-scripts.js
+++ client/user-scripts.js
@@ -1,89 +1,106 @@
 /* *************************
-*** USER SIGNUP ***
-************************** */
+ *** USER SIGNUP ***
+ ************************** */
 function userSignUp() {
-//  console.log('userSignUp Function Called')
+    //  console.log('userSignUp Function Called')
 
-let userEmail = document.getElementById("emailSignup").value;
-let userPass = document.getElementById("pwdSignup").value;
+    let userEmail = document.getElementById("emailSignup").value;
+    let userPass = document.getElementById("pwdSignup").value;
 
-let newUserData = {
-    user: {
-        email: userEmail,
-        password: userPass
-    }
-};
-console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
-fetch(`http://localhost:3000/user/register`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newUserData)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    let token = data.sessionToken;
-    localStorage.setItem("Session Token", token);
-    tokenChecker();
-})
-.catch(err => {
-    console.log(err)
-})
+    let newUserData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+    console.log(`newUserData --> ${newUserData.user.email} ${newUserData.user.password}`);
+    fetch(`http://localhost:3000/user/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUserData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let token = data.sessionToken;
+            localStorage.setItem("SessionToken", token);
+            tokenChecker();
+        })
+        .catch(err => {
+            console.log(err)
+        })
 };
 
 /* *************************
-*** USER LOGIN ***
-************************** */
+ *** USER LOGIN ***
+ ************************** */
 function userLogin() {
-//  console.log('userLogin Function Called')
-let userEmail = document.getElementById("emailLogin").value;
-let userPass = document.getElementById("pwdLogin").value;
-console.log(userEmail, userPass);
+    //  console.log('userLogin Function Called')
+    let userEmail = document.getElementById("emailLogin").value;
+    let userPass = document.getElementById("pwdLogin").value;
+    console.log(userEmail, userPass);
 
-let loggedInUserData = {
-    user: {
-        email: userEmail,
-        password: userPass
-    }
-};
-console.log(loggedInUserData);
-console.log(`loggedInUserData --> ${loggedInUserData.user.email} ${loggedInUserData.user.password}`);
-fetch(`http://localhost:3000/user/login`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(loggedInUserData)
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-    let token = data.sessionToken;
-    localStorage.setItem("Session Token", token);
-    tokenChecker();
-})
-.catch(err => {
-    console.log(err)
-})
+    let loggedInUserData = {
+        user: {
+            email: userEmail,
+            password: userPass
+        }
+    };
+    console.log(loggedInUserData);
+    console.log(`loggedInUserData --> ${loggedInUserData.user.email} ${loggedInUserData.user.password}`);
+    fetch(`http://localhost:3000/user/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loggedInUserData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let token = data.sessionToken;
+            localStorage.setItem("SessionToken", token);
+            tokenChecker();
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 /* *************************
-*** USER LOGOUT ***
-************************** */
+ *** USER LOGOUT ***
+ ************************** */
 function userLogout() {
- console.log('userLogout Function Called')
-localStorage.setItem("Session Token", undefined);
-console.log(`SessionToken --> ${localStorage.SessionToken}`);
-tokenChecker();
+    console.log('userLogout Function Called')
+    localStorage.setItem("SessionToken", undefined);
+    console.log(`SessionToken --> ${localStorage.SessionToken}`);
+    tokenChecker();
 }
 
 
 /* *************************
  *** TOKEN CHECKER FUNCTION ***
-************************** */
+ ************************** */
 function tokenChecker() {
- console.log('tokenChecker Function Called')
+    console.log('tokenChecker Function Called')
+
+    let display = document.getElementById('journals');
+    let header = document.createElement('h5');
+    let accessToken = localStorage.getItem('SessionToken');
+    let alertText = "Log in or sign up to get started!";
+
+    for (let i = 0; i < display.childNodes.length; i++) {
+        display.removeChild(display.firstChild);
+    }
+    if (accessToken === 'undefined') {
+        display.appendChild(header);
+        header.textContent = alertText;
+        header.setAttribute('id', 'defaultLogin');
+    } else {
+        null
+    }
+
 }
-tokenChecker()
+tokenChecker();
